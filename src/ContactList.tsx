79ec0b0d24efe239d251a91537cd5e263dbc3e92
agmo <mor.agnieszka@gmail.com>
2019-10-26 14:41:39 +0200

    Taks 3 - Filter contacts by last name

--- src/ContactList.tsx
+++ src/ContactList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ContactList.css';
 import {IContact} from "./model";
 import Contact from "./Contact";
@@ -9,16 +9,26 @@ interface IContactListProps {
 }
 
 const ContactList: React.FC<IContactListProps> = ({header, contacts}) => {
+    let [name, filterContacts] = useState('');
+    const filterByName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        filterContacts(event.target.value);
+    };
+
     return (
         <div>
             <h2>{header}</h2>
+            <p>Filter by last name (case insensitive):</p>
+            <input type="text" value={name} onChange={filterByName}/>
+
             {
-                contacts.map((contact: IContact) => (
+                contacts
+                    .filter(contact => contact.name.last.toLowerCase().includes(name.toLowerCase()))
+                    .map((contact: IContact) => (
                     <Contact contact={contact} key={contact.id}/>
                 ))
             }
         </div>
     );
-}
+};
 
 export default ContactList;
